ec6ff755a3c9a1108f6e00a4620e1608ace2ab18
Thiago Alves <talves.ads@gmail.com>
2022-05-13 19:10:44 -0300

    basic structure to send request to the API

---
+++ resources/js/api/BaseMethods.js
@@ -0,0 +1,26 @@
+import getData from "./utils/getData";
+import BaseConfigs from "./BaseConfigs";
+
+export default class BaseMethods extends BaseConfigs {
+    baseUrl;
+
+    async index(params) {
+        return this.api.get(this.baseUrl, {params}).then(getData);
+    }
+
+    async store(data) {
+        return this.api.post(this.baseUrl, data).then(getData);
+    }
+
+    async show(id, params) {
+        const url = this.baseUrl + "/" + id;
+
+        return this.api.get(url, {params}).then(getData);
+    }
+
+    async update(id, data) {
+        const url = this.baseUrl + "/" + id;
+
+        return this.api.put(url, data).then(getData);
+    }
+}
